21ef4da094f0cb8e518553e6af209acde8a22d56
RafoM <rafik1999.mkhitaryan@gmail.com>
2021-03-14 17:51:44 -0700

    Updates

---
+++ precache-manifest.5a005eb2dc07cd1bba825e0924dc2f72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3c68a14ddbc99354ba44f2419a8528",
+    "url": "/Project/index.html"
+  },
+  {
+    "revision": "31622dd6797600e360bb",
+    "url": "/Project/static/css/main.07ff5d55.chunk.css"
+  },
+  {
+    "revision": "3975b54353182c7397d8",
+    "url": "/Project/static/js/2.a646e8a2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Project/static/js/2.a646e8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31622dd6797600e360bb",
+    "url": "/Project/static/js/main.0cfe3adf.chunk.js"
+  },
+  {
+    "revision": "0fc480c8e94f7e3acafe",
+    "url": "/Project/static/js/runtime-main.15c15f15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Project/static/media/logo.5d5d9eef.svg"
+  }
+]);
